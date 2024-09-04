5b142b78e6711902aff367873ecadb986c1c2464
toonmaterial <30956192+toonmaterial@users.noreply.github.com>
2024-09-04 20:39:09 +0800

    .

--- SellByBag.lua
+++ SellByBag.lua
@@ -1,29 +1,29 @@
-local function iter(container, slot)
+local function iter(bag, slot)
 	if not MerchantFrame:IsShown() then
 		return
 	end
 
-	if slot > C_Container.GetContainerNumSlots(container) then
+	if slot > C_Container.GetContainerNumSlots(bag) then
 		return
 	end
 
-	if not C_Container.GetContainerItemInfo(container, slot) then
-		return iter(container, slot + 1)
+	if not C_Container.GetContainerItemInfo(bag, slot) then
+		return iter(bag, slot + 1)
 	end
 
-	C_Container.UseContainerItem(container, slot)
+	C_Container.UseContainerItem(bag, slot)
 
 	C_Timer.After(.1, function()
-		iter(container, slot + 1)
+		iter(bag, slot + 1)
 	end)
 end
 
-for container = 0, 5 do
-	local f = _G["ContainerFrame" .. container + 1]
+for i = 1, NUM_TOTAL_BAG_FRAMES do
+	local bag = ContainerFrameContainer.ContainerFrames[i]
 
-	f.PortraitButton:HookScript("OnClick", function()
+	bag.PortraitButton:HookScript("OnClick", function()
 		if IsShiftKeyDown() then
-			iter(container, 1)
+			iter(bag:GetID(), 1)
 		end
 	end)
 end
