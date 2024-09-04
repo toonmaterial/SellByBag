f7de55ab4fadacaaa017dd90446a9ed9f7f07065
toonmaterial <30956192+toonmaterial@users.noreply.github.com>
2024-09-04 11:20:25 +0800

    .

--- SellByBag.lua
+++ SellByBag.lua
@@ -1,11 +1,23 @@
+local function sell(container, slot)
+	slot = slot or 1
+
+	if slot > C_Container.GetContainerNumSlots(container) then
+		return
+	end
+
+	C_Container.UseContainerItem(container, slot)
+
+	C_Timer.NewTimer(.1, function()
+		sell(container, slot + 1)
+	end)
+end
+
 for container = 0, 5 do
 	local f = _G["ContainerFrame" .. container + 1]
 
 	f.PortraitButton:HookScript("OnClick", function()
 		if MerchantFrame:IsShown() and IsShiftKeyDown() then
-			for slot = 1, C_Container.GetContainerNumSlots(container) do
-				C_Container.UseContainerItem(container, slot)
-			end
+			sell(container)
 		end
 	end)
 end
